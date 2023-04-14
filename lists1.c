9a36265c85d20b866516d5c7e8b480d3370fa56a
Syiekei <otworiwycliff88@gmail.com>
2023-04-14 04:51:08 -0700

    lists1.c

---
+++ lists1.c
@@ -0,0 +1,121 @@
+#include "shell.h"
+
+/**
+ * list_len - linked list length determined
+ * @h: first node pointer
+ *
+ * Return: list size
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * list_to_strings - an array strings of the list->str returned
+ * @head: first node pointer
+ *
+ * Return: strings array
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (0);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (0);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (0);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+/**
+ * print_list - elements of a list_t linked list printed
+ * @h: first node pointer
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * node_starts_with - node with string starts with prefix returned
+ * @node: list head pointer
+ * @prefix: match string
+ * @c: match next character after prefix
+ *
+ * Return: node match or 0
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
+ * get_node_index - finds the node index
+ * @head: list head pointer
+ * @node: node pointer
+ *
+ * Return: index of node or -1
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
